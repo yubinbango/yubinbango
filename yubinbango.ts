79b6e99792018821cc5cedeb8408c28f8e9b9d43
小林 照幸 <koba1014@gmail.com>
2015-06-18 02:10:14 +0900

    inputが深い階層にある場合に対処

--- yubinbango.ts
+++ yubinbango.ts
@@ -17,7 +17,12 @@ module YubinBango {
           // 郵便番号の入力欄を取得
           let postalcode = hadr.querySelectorAll('.p-postal-code');
           // 郵便番号入力欄が1つの場合でも3桁-4桁で2つに分かれている場合でも両方に対応するため、それぞれのh-adr内の中の最後のpostal-codeにkeyupイベントを付与する
-          postalcode[postalcode.length - 1].addEventListener("keyup", function() { MicroformatDom.prototype.applyDom(this.parentNode) }, false);
+          postalcode[postalcode.length - 1].addEventListener("keyup", function() {
+            let a = this.parentNode;
+            while (a.tagName !== "FORM"){
+             a = a.parentNode;
+            }
+            MicroformatDom.prototype.applyDom(a) }, false);
         }
       });
     }
